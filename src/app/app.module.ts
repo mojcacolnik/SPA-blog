e1ab72ed39a0817d886ce566b0762819b5310a56
Mojca Colnik <mojca.c13@gmail.com>
2021-12-17 20:51:57 +0100

    chore(): init basic app setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { ContactModule } from './contact/contact.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule, ContactModule, ScullyLibModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
